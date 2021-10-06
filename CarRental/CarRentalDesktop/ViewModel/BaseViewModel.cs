c0f825873cfa65f75f6e90139c48ae4a2cd0738c
IMsMelamory <galya.skrebets@gmail.com>
2021-10-06 23:41:11 +0300

    добавила разметку нормальную

--- CarRental/CarRentalDesktop/ViewModel/BaseViewModel.cs
+++ CarRental/CarRentalDesktop/ViewModel/BaseViewModel.cs
@@ -1,10 +1,15 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 using CarRentalDesktop.Annotations;
 
 namespace CarRentalDesktop.ViewModel
 {
-    public class BaseViewModel: INotifyPropertyChanged
+    public class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
